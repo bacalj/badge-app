37d8ef967abb6b5dfa87ac26c1e4cf3d0ef820eb
Joseph Bacal <bacalj@gmail.com>
2021-04-07 17:17:44 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    hello
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="postcss">
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+</style>>
+
